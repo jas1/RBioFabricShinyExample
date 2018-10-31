0770aeff41fdb718aaad6392d56786ece7a4590e
jas1 <jspairani.registros@gmail.com>
2018-10-31 01:56:22 -0300

    first commit
    ignore, app & readme

---
+++ app.r
@@ -0,0 +1,41 @@
+library(htmlwidgets)
+devtools::install_github("jas1/RBioFabric")
+library(igraph)
+library(igraphdata)
+library(RBioFabric)
+library(shiny)
+
+
+data("karate")
+
+# Define UI for app that draws a biofabric ----
+ui <- fluidPage(
+    
+    # App title ----
+    titlePanel("Hello Biofabric!"),
+    
+    
+    p("sample shinyApp to check if it deploys to shinyapps.io"),
+    p("source at: "),
+    
+    fluidRow(
+        column(width = 12, 
+               # Output: biofabric ----
+               bioFabric_htmlwidgetOutput("output_biofabric_net",
+                                          width = "100%", 
+                                          height = "500px") )
+    )
+)
+
+# server side
+server <- function(input, output,session) {
+
+    # defining biofabric output
+    output$output_biofabric_net <- renderBioFabric_htmlwidget({
+            graph_data <- karate
+            bioFabric_htmlwidget( bioFabric(graph_data),height = 500 )
+        })
+}
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Sample biofabric shiny app
+
+sample shinyApp to check if it deploys to shinyapps.io
+
+related to : https://github.com/timelyportfolio/RBioFabric/issues/3
